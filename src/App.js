f79120430bc140c3006a250d815fc4ca59f8ab30
r14minji <rkdalswl0728@gmail.com>
2021-07-13 20:29:15 +0900

    2.1 Reusable Components with JSX + Props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+function Food({ name, picture }) {
+  return (
+    <div>
+      <h1>I like {name}</h1>
+      <img src={picture} />
+    </div>
+  );
+}
+
+const foodILike = [
+  {
+    name: "Kimchi",
+    image:
+      "https://www.bgw.kr/wp-content/uploads/2019/12/%ED%8F%AC%EA%B8%B0%EA%B9%80%EC%B9%98-1200x1200.png",
+  },
+  {
+    name: "Samgyeopsal",
+    image:
+      "https://lh3.googleusercontent.com/proxy/ifVjqDHulXmbR_gs1P5U_Vuv-vLB4M43YxVr1SxrjLK3mnbt5wPPTaXbUts7z-N7uYfmSGOZBE-yPXRdF38Xv0HLH7tAGT0XTHJyV52Doj7rdqnTNlyd5iLp8SuPMDUxciNMKJ2CZw",
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {foodILike.map((dish) => (
+        <Food name={dish.name} picture={dish.image} />
+      ))}
     </div>
   );
 }
