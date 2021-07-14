5f78eacab29e258b9ea0b5b57dc4f15cc1c8291e
r14minji <rkdalswl0728@gmail.com>
2021-07-14 13:00:26 +0900

    #2.4 Protection with PropTypes

--- src/App.js
+++ src/App.js
@@ -1,32 +1,48 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Food({ name, picture }) {
+const foodILike = [
+  {
+    id: 1,
+    name: "Kimchi",
+    image:
+      "https://www.30ml.store/shopimages/30mlstore/005005000082.jpg?1593064288",
+    rating: 4.5,
+  },
+  {
+    id: 2,
+    name: "Samgyeopsal",
+    image: "https://cdn.mindgil.com/news/photo/202103/70839_7148_1250.jpg",
+    rating: 4.7,
+  },
+];
+
+function Food({ name, picture, rating }) {
   return (
     <div>
       <h1>I like {name}</h1>
-      <img src={picture} />
+      <img src={picture} alt={name} />
+      <p>{rating}/5.0</p>
     </div>
   );
 }
 
-const foodILike = [
-  {
-    name: "Kimchi",
-    image:
-      "https://www.bgw.kr/wp-content/uploads/2019/12/%ED%8F%AC%EA%B8%B0%EA%B9%80%EC%B9%98-1200x1200.png",
-  },
-  {
-    name: "Samgyeopsal",
-    image:
-      "https://lh3.googleusercontent.com/proxy/ifVjqDHulXmbR_gs1P5U_Vuv-vLB4M43YxVr1SxrjLK3mnbt5wPPTaXbUts7z-N7uYfmSGOZBE-yPXRdF38Xv0HLH7tAGT0XTHJyV52Doj7rdqnTNlyd5iLp8SuPMDUxciNMKJ2CZw",
-  },
-];
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number,
+};
 
 function App() {
   return (
     <div>
       {foodILike.map((dish) => (
-        <Food name={dish.name} picture={dish.image} />
+        <Food
+          key={dish.id}
+          name={dish.name}
+          picture={dish.image}
+          rating={dish.rating}
+        />
       ))}
     </div>
   );
